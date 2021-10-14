f4daa1fcd1e89f848e78d11564da15038916da9f
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-10-13 21:01:14 -0300

    creada clase para invites

---
+++ src/ClassLibrary/GestorInvitaciones.cs
@@ -0,0 +1,14 @@
+namespace ChatBot
+{
+    public class GestorInvitaciones
+    {
+        private string[] invitacionesEnviadas = [];
+
+        public string EnviarInvitacion(string numeroObjetico)
+        {
+            return "Enlace";
+        }
+
+        private string
+    }
+}
