f90307b6ed3bd440582d9ab260180772f377ac67
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-10-16 16:10:43 -0300

    implementado gestor de invites inicial, conforme evolucione el chatbot se revisa

--- src/ClassLibrary/GestorInvitaciones.cs
+++ src/ClassLibrary/GestorInvitaciones.cs
@@ -1,14 +1,50 @@
+using System.Collections.Generic;
+using System.Text;
+
 namespace ChatBot
 {
     public class GestorInvitaciones
     {
-        private string[] invitacionesEnviadas = [];
+        private GestorInvitaciones(){}
+        private GestorInvitaciones instancia {get;set;}
 
-        public string EnviarInvitacion(string numeroObjetico)
+        //Lo hago singleton porque solo se precisa una instancia y tiene que guardar un estado (los invites enviados)
+        public GestorInvitaciones GestionadorInvitaciones
         {
-            return "Enlace";
+            get
+            {
+                if (this.instancia == null)
+                {
+                    this.instancia = new GestorInvitaciones();
+                    return this.instancia;
+                }
+                return this.instancia;
+            }
+        }
+        private List<string> invitacionesEnviadas = new List<string>();
+
+        public void EnviarInvitacion(string numeroObjetivo)
+        {
+            //se arma el txt y link y manda al bot
         }
 
-        private string
+        private bool ValidarInvitacion(string Enlace)
+        {
+            if  (invitacionesEnviadas.Contains(Enlace))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private string ArmarMensajeInvitacion(string Enlace)
+        {
+            StringBuilder mensaje = new StringBuilder();
+            mensaje.Append("Has sido invitado a unirte al chatbot de Telegram");
+            mensaje.Append($"Link para unirte y registrarte:");
+            mensaje.Append(Enlace);
+
+            return mensaje.ToString();
+        }
     }
 }
