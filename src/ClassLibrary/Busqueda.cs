1a64446f8d640a4af3f50df13eead8545d51b860
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-10-16 16:30:32 -0300

    añadida clase busqueda, falta checkear tema diccionario y publicacion

---
+++ src/ClassLibrary/Busqueda.cs
@@ -0,0 +1,36 @@
+using System.Collections.Generic;
+
+namespace ChatBot
+{
+    public class Busqueda
+    {
+        public Busqueda Instancia
+        {
+            get
+            {
+                if (this.Instancia == null)
+                {
+                    this.Instancia = new Busqueda();
+                    return this.Instancia;
+                }
+                return this.Instancia;
+            }
+            set{}
+        }
+
+        private Busqueda(){}
+
+        public List<Publicacion> BuscarPublicaciones(Dictionary PublicacionesActivas, string PalabraClave)
+        {
+            List<Publicacion> result = new List<Publicacion>();
+            foreach (string i in PublicacionesActivas.Keys)
+            {
+                if (i == PalabraClave)
+                {
+                    result.Add(PublicacionesActivas[i]);
+                }
+            }
+            return result;
+        }
+    }
+}
