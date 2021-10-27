a458b5c423db51874c1f0be19009e31bae31257e
Marcelo Arrarte <32116762+MarceloArrarte@users.noreply.github.com>
2021-10-27 20:41:54 -0300

    Mejora Importers y Reporte

--- src/Importers/IDatabase.cs
+++ src/Importers/IDatabase.cs
@@ -1,9 +1,11 @@
+using System.Collections.Generic;
+
 namespace Importers {
     public interface IDatabase {
         static IDatabase Instancia {get;}
         void Insertar<T>(T objeto);
         void Actualizar<T>(T objeto);
-        T[] Obtener<T>();
+        List<T> Obtener<T>();
         void Eliminar<T>(T objeto);
     }
 }
