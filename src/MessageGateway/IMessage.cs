0571016a54150b33a32e60aa1ccdb73bbdf26503
Salonso1602 <santiago.ralonso@correo.ucu.edu.uy>
2021-11-15 19:31:37 -0300

    Creacion IMessage y telegram message

---
+++ src/MessageGateway/IMessage.cs
@@ -0,0 +1,17 @@
+//--------------------------------------------------------------------------------
+// <copyright file="IMessage.cs" company="Universidad Católica del Uruguay">
+//     Copyright (c) Programación II. Derechos reservados.
+// </copyright>
+//--------------------------------------------------------------------------------
+
+namespace MessageGateway
+{
+    public interface IMessage
+    {
+        string ChatID { get; }
+
+        string TxtMensaje { get; }
+
+        IMessage ResponderMensaje(string txt);
+    }
+}
