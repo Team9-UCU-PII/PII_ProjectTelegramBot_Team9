408e4948c597b61c0de9dc9b4a2f73fc9912c217
Dana Castroman <danacastroman@gmail.com>
2021-11-23 11:17:01 -0300

    Formulario del menú de acciones de una empresa + el handler correspondiente

---
+++ src/MessageGateway/Forms/MenuEmpresa.cs
@@ -0,0 +1,22 @@
+using MessageGateway.Handlers.MenuEmpresa;
+
+namespace MessageGateway.Forms
+{
+  public class FrmMenuEmpresa : FormularioBase
+  {
+    public FrmMenuEmpresa() : base(new Dictionary<string, string>
+      {
+        {"1", "crear publicacion"},
+        {"2", "modificar publicaciones"},
+        {"3", "generar reportes"},
+        {"4", "configurar cuenta"},
+        {"5", "salir"}
+      })
+    {
+      this.messageHandler =
+        new HandlerMenu(
+          new HandlerOpciones()
+        );
+    }
+  }
+}
---
+++ src/MessageGateway/Handlers/MenuEmpresa/1Menu.cs
@@ -0,0 +1,35 @@
+namespace MessageGateway.Handlers.MenuEmpresa
+{
+  public class HandlerMenuEmpresa : MessageHandlerBase
+  {
+    public HandlerMenuEmpresa(IMessageHandler next = null)
+    : base(new PalabrasClaveHandlers[] { PalabrasClaveHandlers.Inicio }, next)
+    {
+    }
+
+    protected override bool InternalHandle(IMessage message, out string response, out PalabrasClaveHandlers nextHandlerKeyword)
+    {
+      if (this.CanHandler(message))
+      {
+        StringBuilder sb = new StringBuilder();
+        sb.AppendJoin('\n',
+        "Estas son las diferentes acciones que puedes realizar:",
+        "\n",
+        "1. Crear publicación",
+        "2. Modificar publicaciones",
+        "3. Generar reportes",
+        "4. Configurar cuenta",
+        "5. Salir");
+        response = sb.ToString();
+        nextHandlerKeyword = PalabrasClaveHandlers.Opciones;
+        return true;
+      }
+      else
+      {
+        response = string.Empty;
+        nextHandlerKeyword = PalabrasClaveHandlers.Inicio;
+        return false;
+      }
+    }
+  }
+}
